dc34da3e987c4bab253f143cb0319777663a5d0c
kew24 <kaitlyn.westra@vai.org>
2024-01-30 15:32:13 -0500

    take out human-specific formatting (from @jsangalang)

--- R/gene_sets_prepare.R
+++ R/gene_sets_prepare.R
@@ -8,8 +8,9 @@
 # @cell_type - cell type (e.g. Immune system, Liver, Pancreas, Kidney, Eye, Brain)
 #
 
+# Modified gene_sets_prepare
 gene_sets_prepare <- function(path_to_db_file, cell_type){
-  
+
   cell_markers = openxlsx::read.xlsx(path_to_db_file)
   cell_markers = cell_markers[cell_markers$tissueType == cell_type,] 
   cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1)
@@ -17,13 +18,13 @@ gene_sets_prepare <- function(path_to_db_file, cell_type){
   
   # correct gene symbols from the given DB (up-genes)
   cell_markers$geneSymbolmore1 = sapply(1:nrow(cell_markers), function(i){
-    
+
     markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore1[i],",")))
-    markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""])
+    # markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""]) # Only for human
     markers_all = sort(markers_all)
     
     if(length(markers_all) > 0){
-      suppressMessages({markers_all = unique(na.omit(checkGeneSymbols(markers_all)$Suggested.Symbol))})
+      # suppressMessages({markers_all = unique(na.omit(checkGeneSymbols(markers_all, species = "mouse")$Suggested.Symbol))}) # This is used if the symbols are not in the format of HGNC
       paste0(markers_all, collapse=",")
     } else {
       ""
@@ -32,13 +33,13 @@ gene_sets_prepare <- function(path_to_db_file, cell_type){
   
   # correct gene symbols from the given DB (down-genes)
   cell_markers$geneSymbolmore2 = sapply(1:nrow(cell_markers), function(i){
-    
+
     markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore2[i],",")))
-    markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""])
+    # markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""]) # Only for human
     markers_all = sort(markers_all)
     
-    if(length(markers_all) > 0){
-      suppressMessages({markers_all = unique(na.omit(checkGeneSymbols(markers_all)$Suggested.Symbol))})
+    if (length(markers_all) > 0){
+      # suppressMessages({markers_all = unique(na.omit(checkGeneSymbols(markers_all)$Suggested.Symbol))}) # This is used if the symbols are not in the format of HGNC
       paste0(markers_all, collapse=",")
     } else {
       ""
@@ -49,7 +50,7 @@ gene_sets_prepare <- function(path_to_db_file, cell_type){
   cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1)
   cell_markers$geneSymbolmore2 = gsub("///",",",cell_markers$geneSymbolmore2)
   cell_markers$geneSymbolmore2 = gsub(" ","",cell_markers$geneSymbolmore2)
-   
+  
   gs = lapply(1:nrow(cell_markers), function(j) {
     gsub(" ","",unlist(strsplit(toString(cell_markers$geneSymbolmore1[j]),",")))
     })
